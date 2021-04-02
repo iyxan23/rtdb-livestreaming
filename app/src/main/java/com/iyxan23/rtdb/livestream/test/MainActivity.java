b083c3a6fabf939ffff6cd219f8e7e62a26d0de5
Iyxan23 <nurihsanalghifari@gmail.com>
2021-04-02 19:28:23 +0700

    add: Add StreamerActivity and ViewerActivity

--- app/src/main/java/com/iyxan23/rtdb/livestream/test/MainActivity.java
+++ app/src/main/java/com/iyxan23/rtdb/livestream/test/MainActivity.java
@@ -2,7 +2,9 @@ package com.iyxan23.rtdb.livestream.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void chooseStreamer(View view) {
+        startActivity(new Intent(this, StreamerActivity.class));
+    }
+
+    public void chooseViewer(View view) {
+        startActivity(new Intent(this, ViewerActivity.class));
     }
 }
